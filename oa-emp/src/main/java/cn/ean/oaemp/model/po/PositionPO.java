58a90c2b6faef52940a02987bac525419b0c7bf0
B1ackSuit <ean969648839@gmail.com>
2023-03-11 00:10:53 +0800

    JWT工具类，Model部分实体类

---
+++ oa-emp/src/main/java/cn/ean/oaemp/model/po/PositionPO.java
@@ -0,0 +1,53 @@
+package cn.ean.oaemp.model.po;
+
+import cn.afterturn.easypoi.excel.annotation.Excel;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @FileName PositionPO
+ * @Author ean
+ * @Date 2023/3/8 22:00
+ **/
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@EqualsAndHashCode(callSuper = false,of = "positionName")
+@Accessors(chain = true)
+@TableName("oa_emp_position")
+@ApiModel(value="PositionPO对象", description="PositionPO对象")
+public class PositionPO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "pkId")
+    @TableId(value = "pk_id", type = IdType.AUTO)
+    private Integer pkId;
+
+    @ApiModelProperty(value = "职位")
+    @TableField(value = "position_name")
+    // @Excel(name = "职位",width = 15)
+    @NonNull
+    private String positionName;
+
+    @ApiModelProperty(value = "创建时间")
+    @TableField(value = "create_time")
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "Asia/Shanghai")
+    private LocalDateTime createTime;
+
+    @ApiModelProperty(value = "是否启用")
+    @TableField(value = "is_enabled")
+    private Boolean enabled;
+
+
+}
---
+++ oa-emp/src/main/java/cn/ean/oaemp/util/security/JwtUtils.java
@@ -0,0 +1,120 @@
+package cn.ean.oaemp.util.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+/**
+ * @FileName JwtUtils
+ * @Author ean
+ * @Date 2023/3/9 19:45
+ **/
+@Component
+public class JwtUtils {
+
+    /**
+     * 载荷部分包含了一组声明（claims）
+     * "sub"（Subject）：JWT 所代表的实体
+     */
+    private static final String CLAIM_KEY_USERNAME = Claims.SUBJECT;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(String username) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    /**
+     * 验证token是否有效和到期
+     * @param token
+     * @param userDetails
+     * @return
+     */
+    public boolean validateToken(String token, UserDetails userDetails) {
+        String username = getUsernameFromToken(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    public String refreshExpiredToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        String username = claims.getSubject();
+
+        return generateToken(username);
+    }
+
+    public boolean isTokenExpired(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            Date expiration = claims.getExpiration();
+            return expiration.before(new Date());
+        } catch (Exception e) {
+            return true;
+        }
+    }
+
+    /**
+     * 从token中获取登录的用户名
+     * @param token
+     * @return
+     */
+    public String getUsernameFromToken(String token) {
+        String username;
+
+        try {
+            Claims claims = getClaimsFromToken(token);
+            username = claims.getSubject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return username;
+    }
+
+    /**
+     * 从token中获取负载中的声明
+     * @param token
+     * @return
+     */
+    private Claims getClaimsFromToken(String token) {
+
+        Claims claims = null;
+
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException |
+                 UnsupportedJwtException |
+                 MalformedJwtException |
+                 SignatureException |
+                 IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        }
+
+        return claims;
+    }
+
+
+}
