f133ed91a5f591609e46de7c7582791baf5058cd
B1ackSuit <ean969648839@gmail.com>
2023-03-23 14:23:54 +0800

    swagger2的配置退回之前版本

--- oa-emp/src/main/java/cn/ean/oaemp/config/Swagger2Config.java
+++ oa-emp/src/main/java/cn/ean/oaemp/config/Swagger2Config.java
@@ -30,32 +30,30 @@ public class Swagger2Config {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
+                //为当前包下的controller生成api文档
                 .apis(RequestHandlerSelectors.basePackage("cn.ean.oaemp.controller"))
                 .paths(PathSelectors.any())
                 .build()
+                // 添加登录认证
                 .securitySchemes(securitySchemes())
                 .securityContexts(securityContexts());
     }
 
     private ApiInfo apiInfo() {
+        //设置文档信息
         return new ApiInfoBuilder()
                 .title("api接口文档")
                 .description("接口文档")
-                .contact(new Contact("ean", "http://localhost:8081/doc.html", "ean969648839@163.com"))
-                .version("0.0.1-SNAPSHOT")
+                .contact(new Contact("ean", "http:localhost:8081/doc.html", "ean969648839@163.com"))
+                .version("1.0")
                 .build();
     }
 
-    private List<SecurityScheme> securitySchemes(){
-        SecurityScheme scheme = new ApiKey(
-                "Authorization","Authorization","header"
-        );
-        List<SecurityScheme> result = new ArrayList<>();
-        result.add(scheme);
+    private List<ApiKey> securitySchemes(){
         //设置请求头信息
-        // List<ApiKey> result = new ArrayList<>();
-        // ApiKey apiKey = new ApiKey("Authorization","Authorization","header");
-        // result.add(apiKey);
+        List<ApiKey> result = new ArrayList<>();
+        ApiKey apiKey = new ApiKey("Authorization","Authorization","header");
+        result.add(apiKey);
         return result;
     }
 
@@ -82,7 +80,6 @@ public class Swagger2Config {
         result.add(new SecurityReference("Authorization",authorizationScopes));
         return result;
     }
-
 
 
 }
